6ab98d540ad7b0b7d68dcfbc128674ac56c70f81
Rakshit Menpara <deltasquare4@gmail.com>
2019-12-07 18:17:50 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# OS Provisioning Scripts for Computers
+
+This repo is a template to provision Manjaro-based linux distribution to look and work as we want it to, with all the bundled software installed.
+
+## Features
+
+* AUR and yay
+* Desktop applications
+* Mac-ish theme for convenient migration
+* Alacritty + Oh-my-zsh
+* Other dotfiles and misc stuff
+
+## Installation
+
+```
+git clone https://github.com/improwised/os-provision.git
+cd os-provision
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,40 @@
+# Prepare pacman
+sudo mkdir -p /root/.gnupg
+sudo pacman-key --init && sudo pacman-key --populate archlinux manjaro && sudo pacman-key --refresh-keys
+
+# Remove packages
+
+# Upgrade all
+sudo pacman -Syyu
+
+# Install zsh & oh-my-zsh
+sudo pacman -S --noconfirm git zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s $(which zsh)
+# Do logout & login
+
+# Install official packages
+# latte-dock
+sudo pacman -S --noconfirm openssh tk nethogs docker vim synapse samba \
+  alacritty dbeaver baobab simple-scan yay firefox htop peek vlc feh
+
+
+# Install community packages
+yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --save --sudoloop --stats --nocombinedupgrade --batchinstall \
+  brother-mfc-7860dw brscan4 concourse-fly-bin fluxctl-bin gitflow-avh google-chrome kubectl-bin peazip-qt kde-servicemenus-peazip \
+  rslsync slack-desktop sublime-text-dev fasd
+
+# Install custom softwares
+
+
+# Enable docker without sudo
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo systemctl enable docker
+sudo systemctl start docker
+newgrp docker
+
+# Gnome settings
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.shell enabled-extensions 'elementary'
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
