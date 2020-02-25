93bf71f50e293f8c848bdb0cf8037b8bd31381c0
Rakshit Menpara <rakshit@improwised.com>
2020-02-25 20:22:15 +0530

    Sudo

--- install.sh
+++ install.sh
@@ -52,4 +52,4 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize
 yay -S brother-mfc-7860dw brscan4
 sudo systemctl start org.cups.cupsd
 sudo lpadmin -p MFC7860DW -E -v socket://192.168.1.10 -P /usr/share/cups/model/MFC7860DW.ppd
-brsaneconfig4 -a name=Brother_MFC7860DW model=MFC-7860DW ip=192.168.1.10
+sudo brsaneconfig4 -a name=Brother_MFC7860DW model=MFC-7860DW ip=192.168.1.10
